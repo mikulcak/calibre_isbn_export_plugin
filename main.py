b47b77da16008605e365bd12ecb0fb6f3435b9db
Marcus Mikulcak <marcus.mikulcak@tu-berlin.de>
2020-05-15 11:23:58 +0200

    added clipboard export for ISBNs

--- main.py
+++ main.py
@@ -2,8 +2,11 @@
 # vim:fileencoding=UTF-8:ts=4:sw=4:sta:et:sts=4:ai
 from __future__ import absolute_import, division, print_function, unicode_literals
 
-__license__   = 'GPL v3'
-__copyright__ = '2011, Kovid Goyal <kovid@kovidgoyal.net>'
+import os
+
+__license__ = 'GPL v3'
+__copyright__ = '2020, Marcus Mikulcak <marcus.mikulcak@gmail.com>'
+__credits__ = 'Kovid Goyal <kovid@kovidgoyal.net>'
 __docformat__ = 'restructuredtext en'
 
 if False:
@@ -12,13 +15,12 @@ if False:
     # You do not need this code in your plugins
     get_icons = get_resources = None
 
-from PyQt5.Qt import QDialog, QVBoxLayout, QPushButton, QMessageBox, QLabel
+from PyQt5.Qt import QDialog, QVBoxLayout, QPushButton, QMessageBox, QLabel, QApplication
 
 from calibre_plugins.interface_demo.config import prefs
 
 
 class DemoDialog(QDialog):
-
     def __init__(self, gui, icon, do_user_config):
         QDialog.__init__(self, gui)
         self.gui = gui
@@ -37,34 +39,34 @@ class DemoDialog(QDialog):
         self.label = QLabel(prefs['hello_world_msg'])
         self.l.addWidget(self.label)
 
-        self.setWindowTitle('Interface Plugin Demo')
+        self.setWindowTitle('Extract ISBN Plugin')
         self.setWindowIcon(icon)
 
         self.about_button = QPushButton('About', self)
         self.about_button.clicked.connect(self.about)
         self.l.addWidget(self.about_button)
 
-        self.isbn_clipboard_button = QPushButton('Copy ISBN to Clipboard', self)
+        self.isbn_clipboard_button = QPushButton('Copy ISBNs to Clipboard',
+                                                 self)
         self.isbn_clipboard_button.clicked.connect(self.copy_to_clipboard)
         self.l.addWidget(self.isbn_clipboard_button)
 
-        self.marked_button = QPushButton(
-            'Show books with only one format in the calibre GUI', self)
-        self.marked_button.clicked.connect(self.marked)
-        self.l.addWidget(self.marked_button)
+        # self.marked_button = QPushButton(
+        #     'Show books with only one format in the calibre GUI', self)
+        # self.marked_button.clicked.connect(self.marked)
+        # self.l.addWidget(self.marked_button)
 
-        self.view_button = QPushButton(
-            'View the most recently added book', self)
-        self.view_button.clicked.connect(self.view)
-        self.l.addWidget(self.view_button)
+        # self.view_button = QPushButton(
+        #     'View the most recently added book', self)
+        # self.view_button.clicked.connect(self.view)
+        # self.l.addWidget(self.view_button)
 
-        self.update_metadata_button = QPushButton(
-            'Update metadata in a book\'s files', self)
-        self.update_metadata_button.clicked.connect(self.update_metadata)
-        self.l.addWidget(self.update_metadata_button)
+        # self.update_metadata_button = QPushButton(
+        #     'Update metadata in a book\'s files', self)
+        # self.update_metadata_button.clicked.connect(self.update_metadata)
+        # self.l.addWidget(self.update_metadata_button)
 
-        self.conf_button = QPushButton(
-                'Configure this plugin', self)
+        self.conf_button = QPushButton('Configure this plugin', self)
         self.conf_button.clicked.connect(self.config)
         self.l.addWidget(self.conf_button)
 
@@ -74,26 +76,41 @@ class DemoDialog(QDialog):
         from calibre.gui2 import error_dialog, info_dialog
         from calibre.ebooks.metadata.meta import get_metadata
 
-
         # Get currently selected books
         rows = self.gui.library_view.selectionModel().selectedRows()
         if not rows or len(rows) == 0:
-            return error_dialog(self.gui, 'Cannot update metadata',
-                             'No books selected', show=True)
+            return error_dialog(self.gui,
+                                'Cannot update metadata',
+                                'No books selected',
+                                show=True)
         # Map the rows to book ids
         ids = list(map(self.gui.library_view.model().id, rows))
         db = self.db.new_api
+
+        isbn_counter = 0
+        error_counter = 0
+
+        clipboard_export = []
+
         for book_id in ids:
             # print(book_id)
             mi = db.get_metadata(book_id, get_cover=False, cover_as_data=False)
             identifiers = mi.identifiers
-            print(identifiers["isbn"])
-            # for identifier in identifiers:
-                # print(identifier[isbn])
+            if "isbn" in identifiers.keys():
+                clipboard_export.append(identifiers["isbn"])
+                # print(identifiers["isbn"])
+                isbn_counter += 1
+            else:
+                # print("No ISBN in entry")
+                error_counter += 1
+        QApplication.clipboard().setText(os.linesep.join(clipboard_export))
 
-        info_dialog(self, 'Success', 'Copied to clipboard', show=True)
-
-
+        info_dialog(
+            self,
+            'Success',
+            'Copied {} ISBNs to clipboard, encountered {} errors'.format(
+                isbn_counter, error_counter),
+            show=True)
 
     def about(self):
         # Get the about text from a file inside the plugin zip file
@@ -107,75 +124,75 @@ class DemoDialog(QDialog):
         # are not found in the zip file will not be in the returned dictionary.
         text = get_resources('about.txt')
         QMessageBox.about(self, 'About the Interface Plugin Demo',
-                text.decode('utf-8'))
+                          text.decode('utf-8'))
 
-    def marked(self):
-        ''' Show books with only one format '''
-        db = self.db.new_api
-        matched_ids = {book_id for book_id in db.all_book_ids() if len(db.formats(book_id)) == 1}
-        # Mark the records with the matching ids
-        # new_api does not know anything about marked books, so we use the full
-        # db object
-        self.db.set_marked_ids(matched_ids)
+    # def marked(self):
+    #     ''' Show books with only one format '''
+    #     db = self.db.new_api
+    #     matched_ids = {book_id for book_id in db.all_book_ids() if len(db.formats(book_id)) == 1}
+    #     # Mark the records with the matching ids
+    #     # new_api does not know anything about marked books, so we use the full
+    #     # db object
+    #     self.db.set_marked_ids(matched_ids)
 
-        # Tell the GUI to search for all marked records
-        self.gui.search.setEditText('marked:true')
-        self.gui.search.do_search()
+    #     # Tell the GUI to search for all marked records
+    #     self.gui.search.setEditText('marked:true')
+    #     self.gui.search.do_search()
 
-    def view(self):
-        ''' View the most recently added book '''
-        most_recent = most_recent_id = None
-        db = self.db.new_api
-        for book_id, timestamp in db.all_field_for('timestamp', db.all_book_ids()).items():
-            if most_recent is None or timestamp > most_recent:
-                most_recent = timestamp
-                most_recent_id = book_id
+    # def view(self):
+    #     ''' View the most recently added book '''
+    #     most_recent = most_recent_id = None
+    #     db = self.db.new_api
+    #     for book_id, timestamp in db.all_field_for('timestamp', db.all_book_ids()).items():
+    #         if most_recent is None or timestamp > most_recent:
+    #             most_recent = timestamp
+    #             most_recent_id = book_id
 
-        if most_recent_id is not None:
-            # Get a reference to the View plugin
-            view_plugin = self.gui.iactions['View']
-            # Ask the view plugin to launch the viewer for row_number
-            view_plugin._view_calibre_books([most_recent_id])
+    #     if most_recent_id is not None:
+    #         # Get a reference to the View plugin
+    #         view_plugin = self.gui.iactions['View']
+    #         # Ask the view plugin to launch the viewer for row_number
+    #         view_plugin._view_calibre_books([most_recent_id])
 
-    def update_metadata(self):
-        '''
-        Set the metadata in the files in the selected book's record to
-        match the current metadata in the database.
-        '''
-        from calibre.ebooks.metadata.meta import set_metadata
-        from calibre.gui2 import error_dialog, info_dialog
+    # def update_metadata(self):
+    #     '''
+    #     Set the metadata in the files in the selected book's record to
+    #     match the current metadata in the database.
+    #     '''
+    #     from calibre.ebooks.metadata.meta import set_metadata
+    #     from calibre.gui2 import error_dialog, info_dialog
 
-        # Get currently selected books
-        rows = self.gui.library_view.selectionModel().selectedRows()
-        if not rows or len(rows) == 0:
-            return error_dialog(self.gui, 'Cannot update metadata',
-                             'No books selected', show=True)
-        # Map the rows to book ids
-        ids = list(map(self.gui.library_view.model().id, rows))
-        db = self.db.new_api
-        for book_id in ids:
-            # Get the current metadata for this book from the db
-            mi = db.get_metadata(book_id, get_cover=True, cover_as_data=True)
-            fmts = db.formats(book_id)
-            if not fmts:
-                continue
-            for fmt in fmts:
-                fmt = fmt.lower()
-                # Get a python file object for the format. This will be either
-                # an in memory file or a temporary on disk file
-                ffile = db.format(book_id, fmt, as_file=True)
-                ffile.seek(0)
-                # Set metadata in the format
-                set_metadata(ffile, mi, fmt)
-                ffile.seek(0)
-                # Now replace the file in the calibre library with the updated
-                # file. We dont use add_format_with_hooks as the hooks were
-                # already run when the file was first added to calibre.
-                db.add_format(book_id, fmt, ffile, run_hooks=False)
+    #     # Get currently selected books
+    #     rows = self.gui.library_view.selectionModel().selectedRows()
+    #     if not rows or len(rows) == 0:
+    #         return error_dialog(self.gui, 'Cannot update metadata',
+    #                          'No books selected', show=True)
+    #     # Map the rows to book ids
+    #     ids = list(map(self.gui.library_view.model().id, rows))
+    #     db = self.db.new_api
+    #     for book_id in ids:
+    #         # Get the current metadata for this book from the db
+    #         mi = db.get_metadata(book_id, get_cover=True, cover_as_data=True)
+    #         fmts = db.formats(book_id)
+    #         if not fmts:
+    #             continue
+    #         for fmt in fmts:
+    #             fmt = fmt.lower()
+    #             # Get a python file object for the format. This will be either
+    #             # an in memory file or a temporary on disk file
+    #             ffile = db.format(book_id, fmt, as_file=True)
+    #             ffile.seek(0)
+    #             # Set metadata in the format
+    #             set_metadata(ffile, mi, fmt)
+    #             ffile.seek(0)
+    #             # Now replace the file in the calibre library with the updated
+    #             # file. We dont use add_format_with_hooks as the hooks were
+    #             # already run when the file was first added to calibre.
+    #             db.add_format(book_id, fmt, ffile, run_hooks=False)
 
-        info_dialog(self, 'Updated files',
-                'Updated the metadata in the files of %d book(s)'%len(ids),
-                show=True)
+    #     info_dialog(self, 'Updated files',
+    #             'Updated the metadata in the files of %d book(s)'%len(ids),
+    #             show=True)
 
     def config(self):
         self.do_user_config(parent=self)
